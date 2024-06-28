f60125d04ce413c7b0a5f5a8c4528387c8a91ece
boguen <qhrms012@naver.com>
2024-06-28 13:37:08 +0900

    [add] TextMeshPro 물재화량 업데이트

--- Assets/_Scripts/ResourceManager.cs
+++ Assets/_Scripts/ResourceManager.cs
@@ -1,13 +1,25 @@
+using TMPro;
 using UnityEngine;
 
 public class ResourceManager : MonoBehaviour
 {
     public int waterAmount = 0;
+    public TextMeshProUGUI WaterText;
+    void Start()
+    {
+        UpdateWaterText(); // 초기 텍스트 업데이트
+    }
 
     // 물의 양을 증가시키는 메서드
     public void IncreaseWater(int amount)
     {
         waterAmount += amount;
-        Debug.Log("Water increased. Current amount: " + waterAmount);
+        UpdateWaterText();
+    }
+
+    // 텍스트 업데이트 메서드
+    private void UpdateWaterText()
+    {
+        WaterText.text = "Water: " + waterAmount;
     }
 }
