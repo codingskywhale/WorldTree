dcfbdf39116955a96b4529b0a3e0bd517fb6debd
boguen <qhrms012@naver.com>
2024-06-28 20:01:10 +0900

    [add] 레벨 바 추가

--- Assets/_Scripts/ResourceManager.cs
+++ Assets/_Scripts/ResourceManager.cs
@@ -1,25 +1,36 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ResourceManager : MonoBehaviour
 {
     public int waterAmount = 0;
-    public TextMeshProUGUI WaterText;
+    public int currentLevel = 1;
+    public int waterPerLevel = 10; // 레벨 당 필요한 물의 양
+    private const int maxWaterAmount = 200; // 물의 최대 양
+    public TextMeshProUGUI waterText; // 물의 재화량을 표시할 텍스트
+    public TextMeshProUGUI levelText; // 현재 레벨을 표시할 텍스트
+    public Image levelFillImage; // 레벨 바 이미지
+
     void Start()
     {
-        UpdateWaterText(); // 초기 텍스트 업데이트
+        UpdateUI(); // 초기 UI 업데이트
     }
 
     // 물의 양을 증가시키는 메서드
     public void IncreaseWater(int amount)
     {
-        waterAmount += amount;
-        UpdateWaterText();
+        waterAmount += amount;        
+        UpdateUI();
     }
 
-    // 텍스트 업데이트 메서드
-    private void UpdateWaterText()
+    
+
+    // UI를 업데이트하는 메서드
+    private void UpdateUI()
     {
-        WaterText.text = "Water: " + waterAmount;
+        waterText.text = "Water: " + waterAmount;
+        levelText.text = "Level: " + currentLevel;
+        levelFillImage.fillAmount = (float)waterAmount / maxWaterAmount;
     }
 }
