adf69fcf04a1f37f589c474f6cdf8a4edf927aa9
boguen <qhrms012@naver.com>
2024-06-30 20:16:40 +0900

    [add] 클릭당 재화 증가 강화 , 자동 재화 증가 강화 , 게임오브젝트 매니저 버튼 스크롤 관리

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
     public Transform[] targets; // 목표 지점들
     public float speed = 5f; // 이동 속도
     public ResourceManager resourceManager; // ResourceManager 참조
+    public int waterIncreaseAmount = 10; // 나무에 도착했을 때 증가하는 물의 양
 
     private int currentTargetIndex = 0;
     private bool isWaiting = false; // 대기 상태 확인
@@ -32,7 +33,7 @@ public class PlayerMovement : MonoBehaviour
         // 나무 태그를 가진 오브젝트에 도착했는지 확인
         if (target.CompareTag("Tree"))
         {
-            resourceManager.IncreaseWater(10); // 물의 양 증가
+            resourceManager.IncreaseWater(waterIncreaseAmount); // 물의 양 증가
         }
 
         // 물 태그를 가진 오브젝트에 도착했는지 확인
@@ -45,5 +46,11 @@ public class PlayerMovement : MonoBehaviour
 
         // 다음 목표 지점으로 변경
         currentTargetIndex = (currentTargetIndex + 1) % targets.Length;
+    }
+
+    // 버튼 클릭 시 호출될 메서드
+    public void IncreaseWaterAmountByButton()
+    {
+        waterIncreaseAmount += 10; // 나무에 도착했을 때 증가하는 물의 양 10 증가
     }
 }
--- Assets/_Scripts/TouchInputManager.cs
+++ Assets/_Scripts/TouchInputManager.cs
@@ -3,14 +3,20 @@ using UnityEngine;
 public class TouchInputManager : MonoBehaviour
 {
     public ResourceManager resourceManager; // ResourceManager 참조
+    public int IncreaseAmount = 10; // 클릭당 증가할 물의 양 초기값
 
     void Update()
     {
         // 터치 입력 감지
         if (Input.GetMouseButtonDown(0)) // 마우스 왼쪽 버튼 클릭 또는 터치 입력
         {
-            resourceManager.IncreaseWater(10); // 물의 양 증가
+            resourceManager.IncreaseWater(IncreaseAmount); // 물의 양 증가
         }
     }
-}
 
+    // 강화 버튼 클릭 시 호출될 메서드
+    public void UpgradeIncreaseAmount()
+    {
+        IncreaseAmount += 10; // 증가량을 10씩 증가시킴
+    }
+}
